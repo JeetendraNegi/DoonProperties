70a618778430a4f459f1c7eaed1462dcb6a3453c
Jeetendra Singh Negi <jeetendranegi2193@gmail.com>
2024-11-16 07:55:00 +0530

    Update the api port in angular and aupdate the mongo connection string and add the docker compose file

--- DoonProperties-angular-17-client/src/environments/environment.prod.ts
+++ DoonProperties-angular-17-client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7110/api/'
+  apiUrl: "https://localhost:32771/api/",
 };
--- DoonProperties-angular-17-client/src/environments/environment.ts
+++ DoonProperties-angular-17-client/src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7110/api/'
+  apiUrl: "https://localhost:32771/api/",
 };
